2a01d7c5c968d330d3a9b0b7db2633a21d08a689
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-12-15 00:27:44 +0200

    deleted useState and useEffect in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC } from 'react';
 import { lazy } from 'react';
 import { Suspense } from 'react';
 import { Global, css } from '@emotion/react';
